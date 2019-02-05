837e5a6392a636d6124d5738579aed02a85643c0
Wilco Bergacker <wilcobergacker@gmail.com>
2019-02-05 20:17:30 +0100

    add excercise 1

---
+++ 01/wordvalue.py
@@ -0,0 +1,27 @@
+from data import ENGLISH_DICTIONARY, LETTER_SCORES
+
+def load_words():
+    """Load dictionary into a list and return list"""
+    with open(ENGLISH_DICTIONARY) as file:
+        return [word.strip() for word in file.read().split()]
+
+
+def calc_word_value(word):
+    """Calculate the value of the word entered into function
+    using imported constant mapping LETTER_SCORES"""
+    return sum(LETTER_SCORES.get(char.upper(), 0) for char in word)
+
+
+def max_word_value(words=None):
+    """Calculate the word with the max value, can receive a list
+    of words as arg, if none provided uses default DICTIONARY"""
+    if words is None:
+        words = load_words()
+    return max(words, key=calc_word_value)
+
+
+if __name__ == "__main__":
+    pass # run unittests to validate
+
+
+print(max_word_value())
